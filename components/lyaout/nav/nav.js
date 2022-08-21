cf8d62ad28cf174a6dd6fc017e9d9dbcbaecef03
OlekSh <sourceregister@gmail.com>
2022-08-21 13:09:14 +0300

    made mobile menu

--- components/lyaout/nav/nav.js
+++ components/lyaout/nav/nav.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-import Link from 'next/link';
+import NavList from './nav-list';
 import classes from './nav.module.css';
 
 function Nav(props) {
@@ -41,19 +41,7 @@ function Nav(props) {
             <span className={classes.line_second}></span>
             <span className={classes.line_third}></span>
         </div>
-        <ul className={classes.nav_list}>
-          <li><Link href='/posts/business'><a>Бізнес</a></Link></li>
-          <li><Link href='/posts/economy'><a>Економіка</a></Link></li>
-          <li><Link href='/posts/investments'><a>Інвестиції</a></Link></li>
-          <li><Link href='/posts/finance'><a>Фінанси</a></Link></li>
-        </ul>
-        <ul className={classes.nav_list}>
-          <li><Link href='/posts/industry'>
-            <a>Промисловість</a></Link></li>
-          <li><Link href='/posts/e-commerce'><a>Є-комерція</a></Link></li>
-          <li><Link href='/posts/energy'><a>Енергетика</a></Link></li>
-          <li><Link href='/contact'><a>Контакт</a></Link></li>
-        </ul>
+        <NavList />
       </div>
     </>
   )
